9433989ae0abd39297f7349c2bbd98d44ccf74d2
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-05-01 11:53:27 +0200

    Install MDS Towns into directory_country_region

--- app/code/local/MDS/Shipping/sql/collivery_setup/install-0.1.0.php
+++ app/code/local/MDS/Shipping/sql/collivery_setup/install-0.1.0.php
@@ -1,4 +1,24 @@
 <?php
+//******************************** SOAP ********************************
+$soap = new SoapClient("http://www.collivery.co.za/webservice.php?wsdl");
+// Prevent caching of the wsdl
+ini_set("soap.wsdl_cache_enabled", "0");
+// Authenticate
+$authenticate = $soap->Authenticate('demo@collivery.co.za', 'demo', '');
+
+if(!$authenticate['token']) {
+	exit("Authentication Error : ".$authenticate['access']);
+}
+
+$towns = $soap->getTowns(null,$authenticate['token']);
+$town_sql = '';
+
+foreach ($towns['results'] as $key => $value) {
+	$town_sql .= "('ZA', '". addslashes($key) ."', '". addslashes($value) ."'),";
+}
+// Replace last ',' with ';'
+$town_sql = substr($town_sql, 0, -1) . ';';
+//**************************** END OF SOAP ****************************
 
 $installer = $this;
 
@@ -40,6 +60,11 @@ CREATE TABLE IF NOT EXISTS mds_collivery_quote (
   `email` varchar(255) NOT NULL,
   `notes` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;");
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+DELETE FROM `directory_country_region` WHERE `country_id` = 'ZA';
+
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+" . $town_sql);
 
 $installer->endSetup();
