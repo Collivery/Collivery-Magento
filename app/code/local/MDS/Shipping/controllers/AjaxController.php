c4121616cbe90038cd2c53be0a46ed333ceac50e
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-05-01 12:24:14 +0200

    Removed suburbLayoutAction from AjaxController
    Removed suburbLayoutAction from AjaxController as it has been moved
    to pure JS for speed and is no longer in use.

--- app/code/local/MDS/Shipping/controllers/AjaxController.php
+++ app/code/local/MDS/Shipping/controllers/AjaxController.php
@@ -18,18 +18,5 @@ class MDS_Shipping_AjaxController extends Mage_Core_Controller_Front_Action
 			echo '<option value="'. $key .'">'. $value .'</option>';
 		}
 	}
-	
-	public function suburbLayoutAction()
-	{
-		echo '
-		<div class="mds-billing field">
-			<label class="required" for="mds:billing_suburb"><em>*</em>Suburb</label>
-			<div class="input-box">
-				<select class="required-entry" title="Town" name="mds[billing_suburb]" id="mds:billing_suburb" defaultvalue="">
-					<option value="">Please select a Town first</option>
-				</select>
-			</div>
-		</div>';
-	}
-		
+
 }
