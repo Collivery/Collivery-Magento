6a62bdcf5649d6d31d0e3c818b448d253260a167
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-05-02 04:04:37 +0200

    [JS] Make space for Shipping Information
    Changed "billing" to a variable that can be set on function call.
    Changed isZA var to isZA_B for Billing

--- js/mds/mds_collivery.js
+++ js/mds/mds_collivery.js
@@ -2,111 +2,111 @@ var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
 
-	var isZA;
+	var isZA_B;
 
-	function setFields () {
-		$j("label[for='billing\\:region_id']").addClass('required');
-		$j("label[for='billing\\:region_id']").empty();
-		$j("label[for='billing\\:region_id']").append('<em>*</em>Town');
+	function setFields (shipto) {
+		$j("label[for='" + shipto + "\\:region_id']").addClass('required');
+		$j("label[for='" + shipto + "\\:region_id']").empty();
+		$j("label[for='" + shipto + "\\:region_id']").append('<em>*</em>Town');
 		
 		var suburb_html = 
-		'<div class="mds-billing field">' +
-		'	<label class="required" for="mds:billing_suburb"><em>*</em>Suburb</label>' +
+		'<div class="mds-' + shipto + ' field">' +
+		'	<label class="required" for="mds:' + shipto + '_suburb"><em>*</em>Suburb</label>' +
 		'	<div class="input-box">' +
-		'		<select class="required-entry" title="Suburb" name="mds[billing_suburb]" id="mds:billing_suburb" defaultvalue="">' +
+		'		<select class="required-entry" title="Suburb" name="mds[' + shipto + '_suburb]" id="mds:' + shipto + '_suburb" defaultvalue="">' +
 		'			<option value="">Please select a Town first</option>' +
 		'		</select>' +
 		'	</div>' +
 		'</div>';
-		$j("#billing\\:city").parent().parent().parent().prepend(suburb_html);
-		$j('#billing\\:city').parent().parent().hide();
+		$j("#" + shipto + "\\:city").parent().parent().parent().prepend(suburb_html);
+		$j("#" + shipto + "\\:city").parent().parent().hide();
 		
 		var building_html = 
-		'<li class="mds-billing wide">' +
-		'	<label for="mds:billing_building" class="required"><em>*</em>Building Details</label>' +
+		'<li class="mds-' + shipto + ' wide">' +
+		'	<label for="mds:' + shipto + '_building" class="required"><em>*</em>Building Details</label>' +
 		'	<div class="input-box">' +
-		'		<input title="Building Details" name="mds[billing_building]" id="mds:billing_building" value="" class="input-text required-entry" type="text">' +
+		'		<input title="Building Details" name="mds[' + shipto + '_building]" id="mds:' + shipto + '_building" value="" class="input-text required-entry" type="text">' +
 		'	</div>' +
 		'</li>';
-		$j("#billing\\:street1").parent().parent().before(building_html);
+		$j("#" + shipto + "\\:street1").parent().parent().before(building_html);
 		
 		var cptypes_html = 
-		'<div class="mds-billing field">' +
-		'	<label class="required" for="mds:billing_cptypes"><em>*</em>Location Type</label>' +
+		'<div class="mds-' + shipto + ' field">' +
+		'	<label class="required" for="mds:' + shipto + '_cptypes"><em>*</em>Location Type</label>' +
 		'	<div class="input-box">' +
-		'		<select class="required-entry" title="Location Type" name="mds[billing_cptypes]" id="mds:billing_cptypes" defaultvalue="">' +
+		'		<select class="required-entry" title="Location Type" name="mds[' + shipto + '_cptypes]" id="mds:' + shipto + '_cptypes" defaultvalue="">' +
 		'			<option value="">Loading...</option>' +
 		'		</select>' +
 		'	</div>' +
 		'</div>';
-		$j("#billing\\:city").parent().parent().parent().append(cptypes_html);
+		$j("#" + shipto + "\\:city").parent().parent().parent().append(cptypes_html);
 		
 		var cptypes_html = 
-		'<div class="mds-billing field" style="display: none;">' +
+		'<div class="mds-' + shipto + ' field" style="display: none;">' +
 		'	<div class="input-box">' +
-		'		<input name="mds[billing_town]" id="mds:billing_town" value="" class="input-text required-entry" type="text">' +
+		'		<input name="mds[' + shipto + '_town]" id="mds:' + shipto + '_town" value="" class="input-text required-entry" type="text">' +
 		'	</div>' +
 		'</div>';
-		$j("#billing\\:city").parent().parent().parent().append(cptypes_html);
+		$j("#" + shipto + "\\:city").parent().parent().parent().append(cptypes_html);
 	}
 
-	function getSuburbs () {
+	function getSuburbs (shipto) {
 		
-		$j('#mds\\:billing_suburb').empty();
-		$j('#mds\\:billing_suburb').append('<option value="">Loading...</option>');
+		$j("#mds\\:" + shipto + "_suburb").empty();
+		$j("#mds\\:" + shipto + "_suburb").append('<option value="">Loading...</option>');
 		
 		var data = {
-			town	: $j("#billing\\:region_id option:selected").text(),
+			town	: $j("#" + shipto + "\\:region_id option:selected").text(),
 		};
 		jQuery.ajax({
 			type : 'POST',
 			url : "http://localhost/magento/index.php/collivery/ajax/suburb",
 			data : data,
 			complete : function(response){
-				$j('#mds\\:billing_suburb').empty();
-				$j("#mds\\:billing_suburb").append(response['responseText']);
-				$j("#billing\\:city").val($j("#mds\\:billing_suburb option:selected").text());
+				$j("#mds\\:" + shipto + "_suburb").empty();
+				$j("#mds\\:" + shipto + "_suburb").append(response['responseText']);
+				$j("#" + shipto + "\\:city").val($j("#mds\\:" + shipto + "_suburb option:selected").text());
 			}
 		});
 		
 	}
 
-	function getCPTypes () {
+	function getCPTypes (shipto) {
 		
-		$j('#mds\\:billing_cptypes').empty();
-		$j('#mds\\:billing_cptypes').append('<option value="">Loading...</option>');
+		$j("#mds\\:" + shipto + "_cptypes").empty();
+		$j("#mds\\:" + shipto + "_cptypes").append('<option value="">Loading...</option>');
 		
 		jQuery.ajax({
 			url : "http://localhost/magento/index.php/collivery/ajax/cptypes",
 			complete : function(response){
-				$j('#mds\\:billing_cptypes').empty();
-				$j("#mds\\:billing_cptypes").append(response['responseText']);
+				$j("#mds\\:" + shipto + "_cptypes").empty();
+				$j("#mds\\:" + shipto + "_cptypes").append(response['responseText']);
 			}
 		});
 		
 	}
 
-	function setZA () {
-		setFields();
-		getCPTypes();
-		isZA = true;
+	function setZA (shipto) {
+		setFields(shipto);
+		getCPTypes(shipto);
 	}
 
-	function unSetZA () {
-		$j('.mds-billing').remove();
-		$j('#billing\\:city').parent().parent().show();
-		$j("label[for='billing\\:region_id']").removeClass('required');
-		$j("label[for='billing\\:region_id']").empty();
-		$j("label[for='billing\\:region_id']").append('<em style="display: none;">*</em>State/Province');
-		isZA = false;
+	function unSetZA (shipto) {
+		$j('.mds-' + shipto).remove();
+		$j("#" + shipto + "\\:city").parent().parent().show();
+		$j("label[for='" + shipto + "\\:region_id']").removeClass('required');
+		$j("label[for='" + shipto + "\\:region_id']").empty();
+		$j("label[for='" + shipto + "\\:region_id']").append('<em style="display: none;">*</em>State/Province');
 	}
 
 	jQuery('select#billing\\:country_id').live('change', function() {
 		if ($j("#billing\\:country_id").val() == "ZA") {
-			setZA();
+			setZA('billing');
+			isZA_B = true;
 		} else {
-			if (isZA){
-				unSetZA();
+			if (isZA_B){
+				unSetZA('billing');
+				isZA_B = false;
 			}
 		}
 	});
@@ -114,7 +114,7 @@ $j(document).ready(function() {
 	jQuery('select#billing\\:region_id').live('change', function() {
 		if ($j("#billing\\:country_id").val() == "ZA") {
 			$j("#mds\\:billing_town").val($j("#billing\\:region_id option:selected").text());
-			getSuburbs();
+			getSuburbs('billing');
 		}
 	});
 
@@ -123,7 +123,8 @@ $j(document).ready(function() {
 	});
 
 	if ($j("#billing\\:country_id").val() == "ZA") {
-		setZA();
+		setZA('billing');
+		isZA_B = true;
 	}
 
 });
