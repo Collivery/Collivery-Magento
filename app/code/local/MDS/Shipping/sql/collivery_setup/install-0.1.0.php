9fe4b7a6cc3672a39b788a7bdcc736b520c84f4b
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-05-02 02:27:25 +0200

    Automatically add Product Dimention Attributes
    Also changed database layout (Changed in code long ago!)

--- app/code/local/MDS/Shipping/sql/collivery_setup/install-0.1.0.php
+++ app/code/local/MDS/Shipping/sql/collivery_setup/install-0.1.0.php
@@ -22,49 +22,52 @@ $town_sql = substr($town_sql, 0, -1) . ';';
 
 $installer = $this;
 
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
 $installer->startSetup();
 
 $installer->run("
 
 CREATE TABLE IF NOT EXISTS mds_collivery_order (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `town` varchar(16) NOT NULL,
-  `suburb` int(11) NOT NULL,
-  `cptype` int(11) NOT NULL,
-  `company` varchar(255) NOT NULL,
-  `building_details` text NOT NULL,
-  `street_no` varchar(8) NOT NULL,
-  `street_name` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `cell_phone` varchar(16) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `notes` longtext NOT NULL,
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `order_id` int(11) unsigned NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS mds_collivery_quote (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `quote_id` int(11) NOT NULL,
-  `town` varchar(16) NOT NULL,
-  `suburb` int(11) NOT NULL,
-  `cptype` int(11) NOT NULL,
-  `company` varchar(255) NOT NULL,
-  `building_details` text NOT NULL,
-  `street_no` varchar(8) NOT NULL,
-  `street_name` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `cell_phone` varchar(16) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `notes` longtext NOT NULL,
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `quote_id` int(11) unsigned NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `value` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELETE FROM `directory_country_region` WHERE `country_id` = 'ZA';
 
 INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
 " . $town_sql);
 
+$attributes = array('length'=>'Length','width'=>'Width','height'=>'Height');
+
+foreach ($attributes as $key => $value) {
+	$setup->addAttribute('catalog_product', $key, array(
+		'group'         => 'General',
+		'input'         => 'text',
+		'type'          => 'dec',
+		'label'         => $value,
+		'backend'       => '',
+		'visible'       => TRUE,
+		'required'      => TRUE,
+		'user_defined'  => TRUE,
+		'searchable'    => FALSE,
+		'filterable'    => FALSE,
+		'comparable'    => FALSE,
+		'visible_on_front' => FALSE,
+		'visible_in_advanced_search'  => FALSE,
+		'is_html_allowed_on_front' => FALSE,
+		'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+	));
+}
 $installer->endSetup();
