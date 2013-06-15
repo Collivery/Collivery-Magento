e314770315df22b5e37ea23ab02c7a7056c78361
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-06-15 23:12:50 +0200

    Add extra mds fields to order on install

--- app/code/local/MDS/Collivery/sql/collivery_setup/install-0.1.0.php
+++ app/code/local/MDS/Collivery/sql/collivery_setup/install-0.1.0.php
@@ -92,9 +92,13 @@ foreach ($address_attributes as $attributeCode => $data) {
 $installer->run("
 	ALTER TABLE {$this->getTable('sales_flat_quote_address')} ADD COLUMN `mds_building` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL AFTER `fax`;
 	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_building` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL AFTER `fax`;
-	ALTER TABLE {$this->getTable('sales_flat_quote_address')} ADD COLUMN `mds_cptype` INT(11) NULL AFTER `fax`;
-	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_cptype` INT(11) NULL AFTER `fax`;
-
+	ALTER TABLE {$this->getTable('sales_flat_quote_address')} ADD COLUMN `mds_cptype` INT(11) NULL AFTER `mds_building`;
+	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_cptype` INT(11) NULL AFTER `mds_building`;
+	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_address_id` INT(11) NULL AFTER `mds_cptype`;
+	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_contact_id` INT(11) NULL AFTER `mds_cptype`;
+	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_address_hash` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL AFTER `mds_address_id`;
+	ALTER TABLE {$this->getTable('sales_flat_order_address')} ADD COLUMN `mds_contact_hash` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL AFTER `mds_contact_id`;
+	
 	DELETE FROM `directory_country_region` WHERE `country_id` = 'ZA';
 	
 	INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
