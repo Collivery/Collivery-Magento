dd063bf3e69e2986e91d823449aa1ab75c8d707d
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2013-07-04 11:35:27 +0200

    Add Frontend controller to track shipping status

---
+++ app/code/local/MDS/Collivery/controllers/TrackingController.php
@@ -0,0 +1,17 @@
+<?php
+class MDS_Collivery_TrackingController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction()
+	{
+		$waybill = $this->getRequest()->getParam('id');
+		
+		echo "<!DOCTYPE html><html><head><title>MDS Parcel Tracking</title></head><body>";
+		echo "<iframe src=\"http://quote.collivery.co.za/tracking.php";
+		if(isset($waybill)){
+			 echo "?Check=1&waybillNo=$waybill";
+		}
+		echo "\" style=\"width: 100%; height: 100%; border: none;\"></iframe>";
+		echo "</body></html>";
+	}
+
+}
