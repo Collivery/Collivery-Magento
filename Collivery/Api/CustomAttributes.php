9a4aaee64d8a8bec5ddf4e7808ac13ef585f7da4
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-05-22 14:59:13 +0200

    Add class that will be accessed via api to update 'location_type','town' and suburb

---
+++ Collivery/Api/CustomAttributes.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mosa
+ * Date: 2019/05/03
+ * Time: 11:28 AM
+ */
+
+namespace MDS\Collivery\Api;
+
+use Magento\Checkout\Model\Cart;
+use Magento\Customer\Model\Session;
+use Magento\Quote\Api\Data\AddressInterface;
+use Psr\Log\LoggerInterface;
+
+class CustomAttributes
+{
+    private $cart;
+    private $logger;
+    private $session;
+    private $address;
+
+    public function __construct(
+        Session $session,
+        Cart $cart,
+        LoggerInterface $logger,
+        AddressInterface $address
+    ) {
+        $this->cart = $cart;
+        $this->logger = $logger;
+        $this->session = $session;
+        $this->address  = $address;
+    }
+
+    /**
+     * @return void
+     * @throws \Exception
+     */
+    public function insertCustomAttributes()
+    {
+        $quote = $this->cart->getQuote();
+
+        $address = $quote->getShippingAddress();
+        $data = [
+            'location' => $_GET['location_type'],
+            'town' => $_GET['town'],
+            'suburb' => $_GET['suburb']
+        ];
+        $address->load($address->getAddressId())->addData($data);
+        $address->setId($address->getAddressId())->save();
+
+        return;
+    }
+}
