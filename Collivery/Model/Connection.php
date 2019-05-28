8a09da9014d665d4c9d2cee51046f404960277c1
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-05-28 10:05:53 +0200

    Allow connection to accommodate carrier class to connect

--- Collivery/Model/Connection.php
+++ Collivery/Model/Connection.php
@@ -6,15 +6,15 @@ class Connection
 {
     private $_collivery;
 
-    public function __construct()
+    public function __construct($username = null, $password = null)
     {
         $config = [
             'app_name'      => 'Magento', // Application Name
             'app_version'   => '2.3.1', // Application Version
-            'app_host'      => 'Magento ver 2.3.1', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
+            'app_host'      => 'Magento ver. 2.3.1', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
             'app_url'       => '', // URL your site is hosted on
-            'user_email'    => 'api@collivery.co.za',
-            'user_password' => 'api123',
+            'user_email'    => $username ?? 'api@collivery.co.za',
+            'user_password' => $password ?? 'api123',
             'demo'          => false,
         ];
 
