4965ab4996f1228635c23636c6c4a8ac75283754
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-05-28 09:51:00 +0200

    Create a connection class to be used buy class Town, Suburb and Location

---
+++ Collivery/Model/Connection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MDS\Collivery\Model;
+
+class Connection
+{
+    private $_collivery;
+
+    public function __construct()
+    {
+        $config = [
+            'app_name'      => 'Magento', // Application Name
+            'app_version'   => '2.3.1', // Application Version
+            'app_host'      => 'Magento ver 2.3.1', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
+            'app_url'       => '', // URL your site is hosted on
+            'user_email'    => 'api@collivery.co.za',
+            'user_password' => 'api123',
+            'demo'          => false,
+        ];
+
+        $this->_collivery = new MdsCollivery($config);
+    }
+
+    public function getConnection()
+    {
+        return $this->_collivery;
+    }
+}
