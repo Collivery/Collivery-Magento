7c5da3d5f4fe44b2a40fe8782689da8564992b91
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-05-28 09:51:40 +0200

    Use connection class

--- Collivery/Model/Customer/Address/Attribute/Source/Location.php
+++ Collivery/Model/Customer/Address/Attribute/Source/Location.php
@@ -2,27 +2,15 @@
 
 namespace MDS\Collivery\Model\Customer\Address\Attribute\Source;
 
-use MDS\Collivery\Model\MdsCollivery;
+use MDS\Collivery\Model\Connection;
 
 class Location extends \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource
 {
     public $_collivery;
 
-    public function __construct()
+    public function __construct(Connection $collivery)
     {
-
-        $config = [
-            'app_name'      => 'Default App Name', // Application Name
-            'app_version'   => '0.0.1',            // Application Version
-            'app_host'      => '', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
-            'app_url'       => '', // URL your site is hosted on
-            'user_email'    => 'api@collivery.co.za',
-            'user_password' => 'api123',
-            'demo'          => false,
-        ];
-
-        $collivery = new MdsCollivery($config);
-        $this->_collivery = $collivery;
+        $this->_collivery = $collivery->getConnection();
     }
 
     public function getAllOptions($withEmpty = true, $defaultValues = false)
--- Collivery/Model/Customer/Address/Attribute/Source/Suburb.php
+++ Collivery/Model/Customer/Address/Attribute/Source/Suburb.php
@@ -2,7 +2,7 @@
 
 namespace MDS\Collivery\Model\Customer\Address\Attribute\Source;
 
-use MDS\Collivery\Model\MdsCollivery;
+use MDS\Collivery\Model\Connection;
 
 class Suburb extends \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource
 {
@@ -12,20 +12,10 @@ class Suburb extends \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource
     public function __construct($town=null)
     {
         $this->_town = $town;
-
-        $config = [
-            'app_name'      => 'Default App Name', // Application Name
-            'app_version'   => '0.0.1',            // Application Version
-            'app_host'      => '', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
-            'app_url'       => '', // URL your site is hosted on
-            'user_email'    => 'api@collivery.co.za',
-            'user_password' => 'api123',
-            'demo'          => false,
-        ];
-
-        $collivery = new MdsCollivery($config);
-        $this->_collivery = $collivery;
+        $collivery = new Connection();
+        $this->_collivery = $collivery->getConnection();
     }
+
 
     public function getAllOptions($withEmpty = true, $defaultValues = false)
     {
