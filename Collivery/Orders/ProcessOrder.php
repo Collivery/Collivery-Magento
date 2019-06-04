bb3f73fadc454a2df9b29c230a52639fe0f1a973
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-04 10:37:36 +0200

    Change an interface to an Abstract to have method bodies and remove accessive code

--- Collivery/Orders/ProcessOrder.php
+++ Collivery/Orders/ProcessOrder.php
@@ -2,11 +2,76 @@
 
 namespace MDS\Collivery\Orders;
 
-interface ProcessOrder
+use MDS\Collivery\Model\Connection;
+
+abstract class ProcessOrder
 {
-    public function addAddress($params);
-    public function addContactAddress($params);
-    public function validateCollivery($params);
-    public function addCollivery($params);
-    public function acceptWaybill($params);
+    /**
+     * @var \MDS\Collivery\Model\Connection
+     */
+    private $_collivery;
+
+    public function __construct()
+    {
+        $collivery = new Connection();
+        $this->_collivery = $collivery->getConnection();
+    }
+
+    /**
+     * @param $params
+     *
+     * @return array
+     */
+    public function addAddress($params)
+    {
+        return $this->_collivery->addAddress($params);
+    }
+
+    /**
+     * @param $params
+     *
+     * @return int
+     */
+    public function addContactAddress($params)
+    {
+        return $this->_collivery->addContact($params);
+    }
+
+    /**
+     * @param $params
+     *
+     * @return array
+     */
+    public function validateCollivery($params)
+    {
+        return $this->_collivery->validate($params);
+    }
+
+    /**
+     * @param $params
+     *
+     * @return int
+     */
+    public function addCollivery($params)
+    {
+        return $this->_collivery->addCollivery($params);
+    }
+
+    /**
+     * @param $params
+     *
+     * @return array
+     */
+    public function acceptWaybill($params)
+    {
+        return $this->_collivery->acceptCollivery($params);
+    }
+
+    /**
+     * @return array
+     */
+    public function getShopOwnerDetails()
+    {
+        return $this->_collivery->getContacts($this->_collivery->getDefaultAddressId());
+    }
 }
