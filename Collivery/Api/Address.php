74f144c523349bda94205fea1d85a27683cc3ff4
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-12 10:45:54 +0200

    Add api service for fetching and updating billing address for checkout address renderer

---
+++ Collivery/Api/Address.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace MDS\Collivery\Api;
+
+use Magento\Checkout\Model\Cart;
+use MDS\Collivery\Model\Customer\Address\Attribute\Source\Location;
+use MDS\Collivery\Model\Customer\Address\Attribute\Source\Suburb;
+use MDS\Collivery\Model\Customer\Address\Attribute\Source\Town;
+
+class Address
+{
+    private $cart;
+
+    public function __construct(Cart $cart)
+    {
+        $this->cart = $cart;
+    }
+
+    /**
+     * Save Billing Address to quote_address table.
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function updateBillingAddress()
+    {
+        $billingAddress = $this->cart->getQuote()->getBillingAddress();
+
+        $data = [];
+        parse_str($_SERVER['QUERY_STRING'], $data);
+
+        $billingAddress->load($billingAddress->getId())->addData($data);
+        $billingAddress->setId($billingAddress->getId())->save();
+    }
+
+    /**
+     * Get Billing Address from quote_address table.
+     *
+     * @return string
+     */
+    public function getBillingAddress()
+    {
+        $quoteBillingAddress = $this->cart->getQuote()->getBillingAddress();
+        $locationType = Location::getLocationById($quoteBillingAddress->getLocation());
+        $town = Town::getTownById($quoteBillingAddress->getTown());
+        $suburb = Suburb::getSuburbById($quoteBillingAddress->getSuburb());
+        $street = implode(', ', $quoteBillingAddress->getStreet());
+
+        $billingAddress = $quoteBillingAddress->getFirstname() . ' ' . $quoteBillingAddress->getLastname();
+        $billingAddress .= $quoteBillingAddress->getCompany() ? "<br> {$quoteBillingAddress->getCompany()}" : "";
+        $billingAddress .= "<br> $street";
+        $billingAddress .= "<br> $suburb, $locationType";
+        $billingAddress .= "<br>$town, {$quoteBillingAddress->getPostcode()}";
+
+        return $billingAddress;
+    }
+}
