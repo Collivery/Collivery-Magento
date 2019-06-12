f0f710a84ad990728851f8d31fc5bb0ca4f75eb7
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-12 10:07:12 +0200

    replace billing address viewer on checkout with module html page

--- Collivery/view/frontend/requirejs-config.js
+++ Collivery/view/frontend/requirejs-config.js
@@ -1,4 +1,10 @@
 var config = {
+  map: {
+    '*':{
+      'Magento_Checkout/template/billing-address/details.html':
+          'MDS_Collivery/template/billing-address/details.html',
+    }
+  },
   config: {
     mixins: {
       'Magento_Checkout/js/action/set-billing-address': {
