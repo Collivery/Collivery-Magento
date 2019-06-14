efe9c0324fbbe7a58881309af64947b03df070fc
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-14 13:35:06 +0200

    refactor code to separate each event for both shipping and billing form refactor to accommodate window.functions

--- Collivery/view/frontend/web/js/action/set-shipping-information-mixin.js
+++ Collivery/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -7,53 +7,28 @@ define([
 ], function ($, wrapper, url, quote, rateRegistry) {
   'use strict';
 
-  $(document).on('change', "[name='town']", function () {
-    setTimeout(() => getSuburbs($(this).val(), 200));
-    function getSuburbs(townId){
-      $.ajax({
-        url: url.build('rest/V1/mds-collivery/suburbs'),
-        type: "GET",
-        data: { param: townId },
-        dataType: 'json',
-        showLoader: true,
-        success : function(result) {
-          var suburbField = $('select[name="suburb"]');
-          suburbField.empty();
-          $.each(result, function( index) {
-            suburbField.append("<option value="+result[index].value+">"+result[index].label+"</option>");
-          });
-        }
-      });
-      setCustomAttribute();
-    }
+  $(document).on('change', ".form-shipping-address select[name='town']", function () {
+    var element = $('.form-shipping-address select[name="suburb"]');
+    setTimeout(() => window.getSuburbs(element ,$(this).val()), 200);
     $(document).find('input[name="city"]').val('').trigger('keypress');
     $(document).find('div[name="shippingAddress.city"]').hide()
   });
 
   function setCustomAttribute()
   {
-    var locationType = $('[name="location"]').val();
-    var town = $('[name="town"]').val();
-    var suburb = $('[name="suburb"]').val();
-    $.ajax({
-      url: url.build('rest/V1/custom-attributes?town='+town+'&suburb='+suburb+'&location_type='+locationType),
-      type: "POST",
-      contentType: "application/json",
-      showLoader: true,
-      success : () => {
-        var address = quote.shippingAddress();
-        rateRegistry.set(address.getKey(), null);
-        rateRegistry.set(address.getCacheKey(), null);
-        quote.shippingAddress(address);
-      }
-    });
+      var locationType = $('#co-shipping-form select[name="location"]').val();
+      var town = $('#co-shipping-form select[name="town"]').val();
+      var suburb = $('#co-shipping-form select[name="suburb"]').val();
+
+      window.setCustomAttribute('town='+town+'&suburb='+suburb+'&location='+locationType, quote, rateRegistry, true)
+
   }
 
-  $(document).on('change', '[name="suburb"]', function() {
+  $(document).on('change', '.form-shipping-address select[name="suburb"]', function() {
     setCustomAttribute();
   });
 
-  $(document).on('change', '[name="location"]', function() {
+  $(document).on('change', '.form-shipping-address select[name="location"]', function() {
     setCustomAttribute();
   });
 
