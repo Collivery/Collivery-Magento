4e418076c17fd073ade557effd1f895d37a1a417
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-14 13:39:35 +0200

    Remove setting of city visibity to false because it is not helping the validation request city as required

--- Collivery/Plugin/Checkout/Model/LayoutProcessor.php
+++ Collivery/Plugin/Checkout/Model/LayoutProcessor.php
@@ -4,39 +4,6 @@ namespace MDS\Collivery\Plugin\Checkout\Model;
 
 class LayoutProcessor
 {
-    public function __construct(
-        \Magento\Payment\Model\Config $paymentModelConfig
-    ) {
-        $this->paymentModelConfig = $paymentModelConfig;
-    }
-
-    /* disable city from checkout page */
-    public function afterProcess(
-        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
-        array $jsLayout
-    ) {
-        /* For Disable city field from checkout page shipping form */
-        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
-        ['shippingAddress']['children']['shipping-address-fieldset']['children']['city'] = [
-            'visible' => false
-        ];
-
-        $activePayments = $this->paymentModelConfig->getActiveMethods();
-        /* For Disable city field from checkout billing form */
-        if (count($activePayments)) {
-            foreach ($activePayments as $paymentCode => $payment) {
-                $jsLayout['components']['checkout']['children']['steps']['children']
-                ['billing-step']['children']['payment']['children']
-                ['payments-list']['children'][$paymentCode . '-form']['children']
-                ['form-fields']['children']['city'] = [
-                    'visible' => false
-                ];
-            }
-        }
-
-        return $jsLayout;
-    }
-
     public function aroundProcess(\Magento\Checkout\Block\Checkout\LayoutProcessor $subject, \Closure $proceed, $jsLayout)
     {
         $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']
@@ -54,11 +21,5 @@ class LayoutProcessor
         $customJsLayout = $proceed($jsLayout);
 
         return $customJsLayout;
-    }
-
-    public function process(\Magento\Checkout\Block\Checkout\LayoutProcessor $subject, $jsLayout)
-    {
-        unset($jsLayout['components']['checkout']['children']['steps']['city']);
-        return $jsLayout;
     }
 }
