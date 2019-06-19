e8c74e11237b436c1d8b9bdbb7c9894481cb7b37
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-19 15:04:11 +0200

    Refactor code to hide city field

--- Collivery/view/frontend/web/js/action/set-shipping-information-mixin.js
+++ Collivery/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -8,10 +8,11 @@ define([
   'use strict';
 
   $(document).on('change', ".form-shipping-address select[name='town']", function () {
+    quote.shippingAddress().city = 'not-required';
     var element = $('.form-shipping-address select[name="suburb"]');
     setTimeout(() => window.getSuburbs(element ,$(this).val()), 200);
-    $(document).find('input[name="city"]').val('').trigger('keypress');
-    $(document).find('div[name="shippingAddress.city"]').hide()
+    $(document).find('input[name="city"]').empty().trigger('keyup').val('not-required').attr('aria-required', false);
+    $(document).find('div[name="shippingAddress.city"]').hide();
   });
 
   function setCustomAttribute()
@@ -21,7 +22,6 @@ define([
       var suburb = $('#co-shipping-form select[name="suburb"]').val();
 
       window.setCustomAttribute('town='+town+'&suburb='+suburb+'&location='+locationType, quote, rateRegistry, true)
-
   }
 
   $(document).on('change', '.form-shipping-address select[name="suburb"]', function() {
