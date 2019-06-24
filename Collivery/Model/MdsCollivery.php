3cf566277abf87058ee8ca031a7aebcb5ff657c7
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-24 14:47:26 +0200

    update the config

--- Collivery/Model/MdsCollivery.php
+++ Collivery/Model/MdsCollivery.php
@@ -33,9 +33,9 @@ class MdsCollivery
         }
 
         $this->config = (object) [
-            'app_name'      => 'Default App Name', // Application Name
-            'app_version'   => '0.0.1',            // Application Version
-            'app_host'      => '', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
+            'app_name'      => 'Magento', // Application Name
+            'app_version'   => '2.3.1', // Application Version
+            'app_host'      => 'Magento ver. 2.3.1', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
             'app_url'       => '', // URL your site is hosted on
             'user_email'    => 'api@collivery.co.za',
             'user_password' => 'api123',
