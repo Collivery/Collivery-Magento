f40e66ee2808dd5dedee3df961000487f8f27c68
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-06-24 14:40:32 +0200

    override city with selected town.text append street[] with suburb.text

--- Collivery/view/frontend/web/js/action/set-shipping-information-mixin.js
+++ Collivery/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -8,23 +8,23 @@ define([
   'use strict';
 
   $(document).on('change', ".form-shipping-address select[name='town']", function () {
-    quote.shippingAddress().city = 'not-required';
     var element = $('.form-shipping-address select[name="suburb"]');
     setTimeout(() => window.getSuburbs(element ,$(this).val()), 200);
-    $(document).find('input[name="city"]').empty().trigger('keyup').val('not-required').attr('aria-required', false);
-    $(document).find('div[name="shippingAddress.city"]').hide();
   });
 
   function setCustomAttribute()
   {
+    $(document).find('div[name="shippingAddress.city"], #co-shipping-form input[name="street[1]"], #co-shipping-form input[name="street[2]"]').hide();
       var locationType = $('#co-shipping-form select[name="location"]').val();
       var town = $('#co-shipping-form select[name="town"]').val();
       var suburb = $('#co-shipping-form select[name="suburb"]').val();
-
+      var city = $('#co-shipping-form select[name="town"] option:selected').text();
+      $(document).find('#co-shipping-form input[name="city"]').val(city).trigger('focus').trigger('keyup');
       window.setCustomAttribute('town='+town+'&suburb='+suburb+'&location='+locationType, quote, rateRegistry, true)
   }
 
   $(document).on('change', '.form-shipping-address select[name="suburb"]', function() {
+    $(document).find('#co-shipping-form input[name="street[1]').val($('option:selected',this).text()).trigger('focus').trigger('keyup');
     setCustomAttribute();
   });
 
