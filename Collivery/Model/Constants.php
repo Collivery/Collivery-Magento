1daabc9378a1ff9fb96c09b874a99386680a823d
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-07-08 18:39:29 +0200

    Add class for constants

---
+++ Collivery/Model/Constants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mosa
+ * Date: 2019/07/05
+ * Time: 12:20 PM
+ */
+
+namespace MDS\Collivery\Model;
+
+class Constants
+{
+    const LATEST_TIME = '16:00';
+    const DELIVERY_DRIVER_DISPATCHED = 15;
+    const IN_TRANSIT = 9;
+    const RECEIVED_BY_COURIER = 21;
+    const PACKAGE = 2;
+}
