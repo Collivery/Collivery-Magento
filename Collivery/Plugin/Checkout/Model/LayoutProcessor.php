96f8314d68f2b9571b352edbb407452513a8e19b
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-07-09 14:19:51 +0200

    Change code because it is not implementing the sorting of fields

--- Collivery/Plugin/Checkout/Model/LayoutProcessor.php
+++ Collivery/Plugin/Checkout/Model/LayoutProcessor.php
@@ -6,13 +6,19 @@ class LayoutProcessor
 {
     public function aroundProcess(\Magento\Checkout\Block\Checkout\LayoutProcessor $subject, \Closure $proceed, $jsLayout)
     {
-        $layout = $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']
-        ['children']['shippingAddress']['children']['shipping-address-fieldset']
-        ['children'];
+        $fields = [
+            'company' => 50,
+            'country_id' => 60,
+            'town' => 61,
+            'city' => 62,
+            'suburb' => 63
+        ];
 
-        $layout['country_id']['sortOrder'] = 70;
-        $layout['town']['sortOrder'] = 80;
-        $layout['suburb']['sortOrder'] = 90;
+        foreach ($fields as $field => $orderNo) {
+            $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']
+            ['children']['shippingAddress']['children']['shipping-address-fieldset']
+            ['children'][$field]['sortOrder'] = $orderNo;
+        }
 
         $customJsLayout = $proceed($jsLayout);
 
