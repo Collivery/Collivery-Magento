f186693b2e0c8fa07f48c1f572285281fef2e2d3
Tshepiso Tshegosebe <tshepiso@collivery.co.za>
2019-11-04 13:59:39 +0200

    * In my connection get the shoppers's config username and password always so i can only call `new connect()` on every file thats needs to connect to MDS soap class
    * Remove static retrieval of module name and module version
    
    * rename $_collivery to $collivery
    
    * remove fallback to to api@collivery coz its suppose to use the client's api info

--- Collivery/Model/Connection.php
+++ Collivery/Model/Connection.php
@@ -2,27 +2,44 @@
 
 namespace MDS\Collivery\Model;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\ProductMetadataInterface;
+use Magento\Config\Model\Config;
+
 class Connection
 {
-    private $_collivery;
+    private $collivery;
+    private $objectManager;
+    private $config;
 
-    public function __construct($username = null, $password = null)
+    const USERNAME_XML_CONFIG_PATH = 'carriers/collivery/username';
+    const PASSWORD_XML_CONFIG_PATH = 'carriers/collivery/password';
+    public function __construct()
     {
-        $config = [
-            'app_name'      => 'Magento', // Application Name
-            'app_version'   => '2.3.1', // Application Version
-            'app_host'      => 'Magento ver. 2.3.1', // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
+        $this->objectManager = ObjectManager::getInstance();
+        $productMetadata = $this->objectManager->get(ProductMetadataInterface::class);
+        $config = $this->objectManager->get(Config::class);
+        $username = $config->getConfigDataValue(self::USERNAME_XML_CONFIG_PATH);
+        $password = $config->getConfigDataValue(self::PASSWORD_XML_CONFIG_PATH);
+
+        $this->config = [
+            'app_name'      => $productMetadata->getName(), // Application Name
+            'app_version'   =>  $productMetadata->getVersion(), // Application Version
+            'app_host'      => "{$productMetadata->getName()} Version. {$productMetadata->getVersion()}", // Framework/CMS name and version, eg 'Wordpress 3.8.1 WooCommerce 2.0.20' / 'Joomla! 2.5.17 VirtueMart 2.0.26d'
             'app_url'       => '', // URL your site is hosted on
-            'user_email'    => $username ?? 'api@collivery.co.za',
-            'user_password' => $password ?? 'api123',
+            'user_email'    => $username,
+            'user_password' => $password,
             'demo'          => false,
         ];
 
-        $this->_collivery = new MdsCollivery($config);
+        $this->collivery = new MdsCollivery($this->config);
     }
 
+    /**
+     * @return MdsCollivery
+     */
     public function getConnection()
     {
-        return $this->_collivery;
+        return $this->collivery;
     }
 }
